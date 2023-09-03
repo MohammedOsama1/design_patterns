8b768343cac8a1f12d2f7a7d4040ce6572f1c8a0
Mohammed-Osama <mallourd16@yahoo.com>
2023-09-03 15:49:21 +0300

    Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,44 @@
+﻿
+interface ITarget
+{
+    void Request();
+}
+
+// class you want to adapt
+class Adaptee
+{
+    public void SpecificRequest()
+    {
+        Console.WriteLine("Adaptee'srequest.");
+    }
+}
+
+
+class Adapter : ITarget
+{
+    private Adaptee adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        this.adaptee = adaptee;
+    }
+
+    public void Request()
+    {
+        adaptee.SpecificRequest();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Adaptee adaptee = new Adaptee();
+
+        var adapter = new Adapter(adaptee);
+
+        adapter.Request();
+
+        Console.ReadLine();
+    }
+}
